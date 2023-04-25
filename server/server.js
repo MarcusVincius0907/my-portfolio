3ab19f98d986ce4f765f1693ed44e602d543550a
unknown <mvleite0908@gmail.com>
2023-04-24 21:50:37 -0300

    feat: adding server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+
+app.use("/assets", express.static(__dirname + "/dist/assets"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+const frontPort = process.env.FRONT_PORT || 8080;
+app.listen(frontPort, () => {
+  console.log(`Server listening on http://localhost:${frontPort}`);
+});
